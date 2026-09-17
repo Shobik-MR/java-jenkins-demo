4d9024a79a512ad768e68c5956b4be0e9ba1fffc
Shobik-don <shobikshobik504@gmail.com>
2026-09-17 15:09:28 +0530

    failure

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int a = 10;
+        int a = 100;
         int b = 20;
 
-        int sum = a + b;
+        int sum = a +b ;
 
         System.out.println("Java Jenkins Demo");
         System.out.println("Sum = " + sum);
